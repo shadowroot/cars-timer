2315d487b3ba9eed5faaeb787c537618c63484f1
jonnyATroot <jonny@192.168.140.204>
2012-03-31 20:22:28 +0200

    gui completed STATUS: WORKING TODO : Zavod

--- src/cars/Cars.java
+++ src/cars/Cars.java
@@ -3,16 +3,16 @@
  * and open the template in the editor.
  */
 package cars;
+import com.sun.xml.internal.ws.util.StringUtils;
 import java.io.*;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 
 /**
  *
  * @author jonny
  */
 public class Cars {
-private static String[] credentials;
+private static String[][] credentials;
+private static String[][] tmpcsv=null;
 private static racers racers = null;
 private static mainFrame mainFrame = null;
 private static String csv = new String();
@@ -21,10 +21,14 @@ private static serial serial = null;
 private static char[] tmp = null;
 private static int maxSize = 0x800;
 private static boolean debug = true;
+private static String[] cols=null;
+private static int id=0;
     
     public static void putCredentials(String[] str) throws IOException{
-        credentials = str;
-        writeCSV(credentials);
+        tmpcsv = new String[credentials.length+1][3];
+        credentials = tmpcsv;
+        credentials[credentials.length-1] = str;
+        writeCSV(str);
     }
     
     public static void readCSV(String str) throws IOException{
@@ -35,6 +39,31 @@ private static boolean debug = true;
             bReader.read(tmp);
         }
         csv = tmp.toString();
+        char[] buf = new char[csv.length()];
+        buf=csv.toCharArray();
+        int size = 0;
+        for(int i=0;i<buf.length;i++){
+            if(buf[i] == ','){
+                size++;
+            }
+        }
+        cols=new String[size];
+        cols = csv.split(",");
+        id=0;
+        for(int i = 0;i<cols.length;i+=3){
+           tmpcsv = new String[credentials.length+1][3];
+            credentials = tmpcsv;
+            for(int u=0;u<3;u++){
+                credentials[id][u] = cols[i+u];
+            }
+            id++;
+        }
+    }
+    /**
+     * Find out if checkboxes with racers is check or not.
+     */
+    public static void determineRacers(int[] racInt){
+        
     }
     
     public static void writeCSV(String[] str) throws IOException{
@@ -70,5 +99,11 @@ private static boolean debug = true;
         racers = new racers();
         
         racers.show();
+    }
+    public static void racerList(){
+        
+    }
+    public static void newRace(){
+        
     }
 }
