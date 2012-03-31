8e1d579c3df2b8636bc7b09b1f96d9c4c861b31b
jonnyATroot <jonny@192.168.140.204>
2012-04-01 01:35:51 +0200

    gui completed STATUS: WORKING TODO : Zavod

--- src/cars/Cars.java
+++ src/cars/Cars.java
@@ -4,7 +4,11 @@
  */
 package cars;
 import com.sun.xml.internal.ws.util.StringUtils;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.Date;
+import javax.swing.Timer;
 
 /**
  *
@@ -12,7 +16,9 @@ import java.io.*;
  */
 public class Cars {
 private static String[][] credentials;
+private static String[][] racersReady = null;
 private static String[][] tmpcsv=null;
+private static int[] positions = null;
 private static racers racers = null;
 private static mainFrame mainFrame = null;
 private static String csv = new String();
@@ -23,6 +29,14 @@ private static int maxSize = 0x800;
 private static boolean debug = true;
 private static String[] cols=null;
 private static int id=0;
+private static Date date=null;
+private static long start_time=0;
+private static long[] times=null;
+private static long now_time=0;
+
+    public Cars() throws IOException{
+        initRacers();
+    }
     
     public static void putCredentials(String[] str) throws IOException{
         tmpcsv = new String[credentials.length+1][3];
@@ -35,15 +49,19 @@ private static int id=0;
         
         tmp = new char[maxSize];
         FileReader fstream = new FileReader(str);
-        try (BufferedReader bReader = new BufferedReader(fstream)) {
+        try {
+            BufferedReader bReader = new BufferedReader(fstream);
             bReader.read(tmp);
+        }
+        catch (Exception e){
+            e.printStackTrace();
         }
         csv = tmp.toString();
         char[] buf = new char[csv.length()];
         buf=csv.toCharArray();
         int size = 0;
         for(int i=0;i<buf.length;i++){
-            if(buf[i] == ','){
+            if(buf[i] == (char)','){
                 size++;
             }
         }
@@ -59,11 +77,17 @@ private static int id=0;
             id++;
         }
     }
+    
+    
     /**
      * Find out if checkboxes with racers is check or not.
      */
     public static void determineRacers(int[] racInt){
-        
+       
+       racersReady=new String[racInt.length][3];
+       for(int i=0;i<racInt.length;i++){
+           racersReady[i]=credentials[racInt[i]];
+       }
     }
     
     public static void writeCSV(String[] str) throws IOException{
@@ -78,7 +102,10 @@ private static int id=0;
         
     }
     public static void initRacers() throws IOException{
+        date = new Date();
+        System.out.println(date.toString());
         readCSV(csvFile);
+        
     }
     
     /**
@@ -87,23 +114,37 @@ private static int id=0;
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         //serial = new serial();
-        mainFrame = new mainFrame();
-        mainFrame.show();
         initRacers();
+        mainFrame = new mainFrame();
+        /*
         if(debug){
             test t = new test();
         }
+        */
         
     }
     public static void newRaces(){
         racers = new racers();
-        
-        racers.show();
     }
     public static void racerList(){
         
     }
     public static void newRace(){
+        Race r = new Race();
+        Timer t = new Timer(1000,new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateGraphics();
+            }
+        });
+        t.start();
+    }
+    public static void breakLine(){
+        date=new Date();
+        now_time = date.getTime();
         
+    }
+    public static void updateGraphics(){
+        Race.updatePanels(credentials, times);
     }
 }
